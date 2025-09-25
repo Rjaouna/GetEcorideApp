a93583883e5d1c8f583ec8e99b23ea05ce801fdb
ridouanej <jaouna.ridouane@gmail.com>
2025-09-25 15:12:41 +0200

    feat/(Authentification): Configuration de la base de donnees sur .ev --  lancer les migration : ok --- créé les fixtures pour 4 users admin, emloye, passager et driver -- lancer les fixture : ok

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,46 @@
+<?php
+// src/DataFixtures/AppFixtures.php
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        // 1) ADMIN
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'Admin@123'));
+        $manager->persist($admin);
+
+        // 2) EMPLOYER (ROLE_EMLOYER)
+        $employer = new User();
+        $employer->setEmail('employer@example.com');
+        $employer->setRoles(['ROLE_EMLOYER']); // orthographe voulue
+        $employer->setPassword($this->passwordHasher->hashPassword($employer, 'Employer@123'));
+        $manager->persist($employer);
+
+        // 3) PASSAGER (ROLE_PASSAGER)
+        $passager = new User();
+        $passager->setEmail('passager@example.com');
+        $passager->setRoles(['ROLE_PASSAGER']);
+        $passager->setPassword($this->passwordHasher->hashPassword($passager, 'Passager@123'));
+        $manager->persist($passager);
+
+        // 4) DRIVER (ROLE_DRIVER)
+        $driver = new User();
+        $driver->setEmail('driver@example.com');
+        $driver->setRoles(['ROLE_DRIVER']);
+        $driver->setPassword($this->passwordHasher->hashPassword($driver, 'Driver@123'));
+        $manager->persist($driver);
+
+        $manager->flush();
+    }
+}
