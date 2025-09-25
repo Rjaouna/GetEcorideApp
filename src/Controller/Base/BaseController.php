6f34f773e0790525d6670a426e42d0a032a92852
ridouanej <jaouna.ridouane@gmail.com>
2025-09-25 14:53:27 +0200

    feat/(Authentification): Ajouter le formulaire de connexion et implimenter la logique de connexion dans BaseController --- redirection selon le role de l'user

--- src/Controller/Base/BaseController.php
+++ src/Controller/Base/BaseController.php
@@ -1,9 +1,18 @@
-<?php
-
+<?php // src/Controller/Base/BaseController.php
 namespace App\Controller\Base;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class BaseController extends AbstractController {}
+class BaseController extends AbstractController
+{
+    /** Redirige selon le rôle courant */
+    protected function redirectByRole(): RedirectResponse
+    {
+        if ($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_EMPLOYE')) {
+            return $this->redirectToRoute('app_admin_dashboard');   // ← route admin
+        }
+
+        return $this->redirectToRoute('app_front_home');              // ← route publique/utilisateur
+    }
+}
---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Controller\Base\BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends BaseController
+{
+    #[Route('/front/home', name: 'app_front_home')]
+    public function index(): Response
+    {
+        if ($this->isGranted('ROLE_ADMIN') || $this->isGranted('ROLE_EMPLOYE')) {
+            return $this->redirectByRole();
+        }
+
+        return $this->render('front/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
