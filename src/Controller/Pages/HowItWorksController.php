87fd1610eb9580e55bbc2b95a2d8802c43d02cfb
ridouanej <jaouna.ridouane@gmail.com>
2025-11-04 10:14:55 +0100

    Page how it works

---
+++ src/Controller/Pages/HowItWorksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HowItWorksController extends AbstractController
+{
+    #[Route('/pages/how/it/works', name: 'app_how_it_works')]
+    public function index(): Response
+    {
+        return $this->render('pages/how_it_works/index.html.twig', [
+            'controller_name' => 'HowItWorksController',
+        ]);
+    }
+}
